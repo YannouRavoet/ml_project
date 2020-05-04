67bc21b2478b3db82cb1d88592203a622bfd7db6
yannouravoet <yannou.ravoet@gmail.com>
2020-05-04 15:32:44 +0200

    saving intermediate cfr and cfr+ training iterations from laptop 3 (750K iterations)

--- src/POKERS/leduc_poker/main_leduc_cfr.py
+++ src/POKERS/leduc_poker/main_leduc_cfr.py
@@ -5,10 +5,11 @@ import utils_poker
 
 def train_policies(game, iterations=0):
     # utils_poker.CFRPlus_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
-    utils_poker.CFR_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
+    # utils_poker.CFR_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
     # utils_poker.XFP_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
     # utils_poker.PG_Solving(game, iterations=iterations, save_every=10000, save_prefix='temp')
     # utils_poker.NFSP_Solving(game, iterations=iterations, save_every=10000, save_prefix='temp')
+    utils_poker.DCFR_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp_111', a=1, b=1, g=1)
     return
 
 
---
+++ src/POKERS/leduc_poker/main_leduc_dcfr.py
@@ -0,0 +1,29 @@
+import pyspiel
+from absl import app
+import utils_poker
+
+
+def train_policies(game, iterations=0):
+    # utils_poker.CFRPlus_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
+    # utils_poker.CFR_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
+    # utils_poker.XFP_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
+    # utils_poker.PG_Solving(game, iterations=iterations, save_every=10000, save_prefix='temp')
+    # utils_poker.NFSP_Solving(game, iterations=iterations, save_every=10000, save_prefix='temp')
+    utils_poker.DCFR_Solving(game, iterations=iterations, save_every=1000, save_prefix='temp')
+    return
+
+
+def main(_):
+    game = pyspiel.load_game("leduc_poker")  # kuhn_poker or leduc_poker
+
+    # TRAINING
+    n = int(1e7)
+    train_policies(game, n)
+
+    # TESTING
+    # utils_poker.plot_policies(game, {'CFR': 'CFR/temp/', 'CFR+': 'CFRPlus/temp/'})
+
+
+
+if __name__ == "__main__":
+    app.run(main)
